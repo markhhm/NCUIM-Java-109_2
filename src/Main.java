2d202f7b684b530284908781e05ad5b5bbaef69b
Hsiang-Ming Hung <bjergsen@24-216.dorm.ncu.edu.tw>
2021-06-08 03:50:00 +0800

    Menu update

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 import java.util.ArrayList;
 public class Main {
-    static ArrayList<String> Single_Meal = new ArrayList<String>();
-    static ArrayList<String> Set_Meal = new ArrayList<String>();
+    static ArrayList<menuType> Single_Menu = new ArrayList<menuType>();
     public static void main(String[] args) {
-        Single_Meal.addAll(new ReadCSV("Single_meal.csv").csvArrayList);
-        Set_Meal.addAll(new ReadCSV("Set_meal.csv").csvArrayList);
-        System.out.println(Set_Meal.get(5));
+        new Menu(Single_Menu);
+        System.out.println(Single_Menu.get(2).name);
     }
-
 }
--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class User{
+public class Test{
 
 	public static void main(String[] args){
 
