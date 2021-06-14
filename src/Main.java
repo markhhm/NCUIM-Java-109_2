b7f65f78eacae464d872069d4833c454dad26161
mark0324 <captainhung096@gmail.com>
2021-06-14 16:22:28 +0800

    remove UserClass

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 import java.util.ArrayList;
 public class Main {
-    static ArrayList<singleType> Single_Menu = new ArrayList<singleType>();
-    static ArrayList<setType> Set_Menu = new ArrayList<setType>();
-    public static void main(String[] args) {
-        new MenuData(Single_Menu, Set_Menu);
-        System.out.println(Single_Menu.get(3).getPrice());
-        System.out.println(Set_Menu.get(2).getDrink().getName());
+    public static void main(String [] args){
+        Menu m = new Menu();
+        m.Menu();
     }
 }
--- src/UserClass.java
+++
@@ -1,7 +0,0 @@
-public class UserClass{
-
-    public static void main(String [] args){
-        Menu m = new Menu();
-        m.Menu();
-    }
-}
