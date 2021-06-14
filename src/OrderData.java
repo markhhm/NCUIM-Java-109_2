bbb7713947efa4757aed29a81b0383233f1f931f
mark0324 <captainhung096@gmail.com>
2021-06-15 07:15:16 +0800

    format doc

--- src/OrderData.java
+++ src/OrderData.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
 public class OrderData {
 
-    static int Singlecount[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };// 單點各餐點數量
-    static int Setcount[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };// 套餐各餐點數量
+    static int[] Singlecount = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };// 單點各餐點數量
+    static int[] Setcount = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };// 套餐各餐點數量
     static String CouponCode;
 }
 
